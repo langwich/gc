3666435485a3a863d610bbd96d00fe02e21a8a96
parrt <parrt@cs.usfca.edu>
2015-10-08 17:51:04 -0700

    (stash)

--- mark-and-compact/gc.c
+++ mark-and-compact/gc.c
@@ -13,9 +13,9 @@ static heap_object **_roots[MAX_ROOTS];
 static int num_roots = 0; /* index of next free space in _roots for a root */
 
 static int heap_size;
-static byte *start_of_heap;
-static byte *end_of_heap;
-static byte *next_free;
+static uint8_t *start_of_heap;
+static uint8_t *end_of_heap;
+static uint8_t *next_free;
 
 // temp array; result of mark operation
 static heap_object *live_objects[MAX_OBJECTS];
@@ -27,7 +27,7 @@ static void gc_sweep();
 
 static int  gc_object_size(heap_object *p);
 static void gc_compact_object_list();
-static void *gc_alloc_space(int size);
+static void *gc_alloc_space(size_t size);
 static void gc_dump();
 static bool gc_in_heap(heap_object *p);
 static char *gc_viz_heap();
@@ -94,31 +94,31 @@ void gc() {
     int i;
     for (i = 0; i < num_live_objects; i++) {
         heap_object *p = live_objects[i];
-        p->header.forwarded = gc_alloc_space(gc_object_size(p));
+        p->forwarded = gc_alloc_space(gc_object_size(p));
     }
 
     // alter roots that point to live objects
     for (i = 0; i < num_roots; i++) {
         if (DEBUG) printf("move root[%d]=%p\n", i, _roots[i]);
         heap_object *p = *_roots[i];
-        if (p != NULL && p->header.marked) {
-            *_roots[i] = p->header.forwarded; // move root to new address
+        if (p != NULL && p->marked) {
+            *_roots[i] = p->forwarded; // move root to new address
         }
     }
 
     // alter fields; walk all live objects and set their ptr fields
     for (i = 0; i < num_live_objects; i++) {
         heap_object *p = live_objects[i];
-        p->header.marked = 0;
+        p->marked = 0;
         int f;
-        if (DEBUG) printf("move ptr fields of %s@%p\n", p->header.metaclass->name, p);
-        for (f = 0; f < p->header.metaclass->num_fields; f++) {
-            int offset_of_ptr_field = p->header.metaclass->field_offsets[f];
-            byte *ptr_to_ptr_field = ((byte *) p) + offset_of_ptr_field;
+        if (DEBUG) printf("move ptr fields of %s@%p\n", p->metaclass->name, p);
+        for (f = 0; f < p->metaclass->num_fields; f++) {
+            int offset_of_ptr_field = p->metaclass->field_offsets[f];
+            uint8_t *ptr_to_ptr_field = ((uint8_t *) p) + offset_of_ptr_field;
             heap_object **ptr_to_obj_ptr_field = (heap_object **) ptr_to_ptr_field;
             heap_object *target_obj = *ptr_to_obj_ptr_field;
             if (target_obj != NULL) {
-                *ptr_to_obj_ptr_field = target_obj->header.forwarded;
+                *ptr_to_obj_ptr_field = target_obj->forwarded;
             }
         }
     }
@@ -126,27 +126,29 @@ void gc() {
     // move objects to compact heap
     for (i = 0; i < num_live_objects; i++) {
         heap_object *p = live_objects[i];
-        memcpy(p->header.forwarded, p, gc_object_size(p));
+        memcpy(p->forwarded, p, gc_object_size(p));
     }
 }
 
-heap_object *gc_alloc(ClassDescriptor *class) {
-    heap_object *p = gc_alloc_space(class->size);
-    memset(p, 0, class->size);
-    p->header.metaclass = class;
+extern heap_object *gc_alloc(size_t size, void (*chase_ptrs)(struct _heap_object *p)) {
+    heap_object *p = gc_alloc_space(size);
+
+    memset(p, 0, size+sizeof(heap_object));
+    p->size = (uint32_t)size;
+    p->chase_ptrs = chase_ptrs;
     return p; // spend hour looking for bug; forgot this
 }
 
-String *gc_alloc_string(int size) {
-    String *s;
-    /* size for struct String, the String itself, and null char */
-    s = (String *) gc_alloc_space(sizeof (String) + size + 1);
-    s->header.metaclass = &String_metaclass;
-    s->header.marked = 0;
-    memset(s->str, 0, size);
-    s->length = size;
-    return s;
-}
+//String *gc_alloc_string(int size) {
+//    String *s;
+//    /* size for struct String, the String itself, and null char */
+//    s = (String *) gc_alloc_space(sizeof (String) + size + 1);
+//    s->metaclass = &String_metaclass;
+//    s->marked = 0;
+//    memset(s->str, 0, size);
+//    s->length = size;
+//    return s;
+//}
 
 int gc_num_roots() {
     return num_roots;
@@ -180,12 +182,12 @@ char *gc_get_state() {
             free(s);
             {
                 // print ptr fields
-                if ( p->header.metaclass->num_fields>0 ) {
+                if ( p->metaclass->num_fields>0 ) {
                     charbuf_add_str(&state, "->[");
                     int i;
-                    for (i = 0; i < p->header.metaclass->num_fields; i++) {
-                        int offset_of_ptr_field = p->header.metaclass->field_offsets[i];
-                        byte *ptr_to_ptr_field = ((byte *) p) + offset_of_ptr_field;
+                    for (i = 0; i < p->metaclass->num_fields; i++) {
+                        int offset_of_ptr_field = p->metaclass->field_offsets[i];
+                        uint8_t *ptr_to_ptr_field = ((uint8_t *) p) + offset_of_ptr_field;
                         heap_object **obj_ptr_to_ptr_field = (heap_object **) ptr_to_ptr_field;
                         heap_object *target_obj = *obj_ptr_to_ptr_field;
                         if ( i>0 ) charbuf_add(&state, ',');
@@ -212,7 +214,7 @@ char *gc_get_state() {
 
 
 
-/* Walk all roots and traverse object graph. Mark all p->header.mark=true for
+/* Walk all roots and traverse object graph. Mark all p->mark=true for
    reachable p.  Fill live_objects[], leaving num_live_objects set at number of live.
  */
 static void gc_mark_live() {
@@ -221,7 +223,7 @@ static void gc_mark_live() {
         if (DEBUG) printf("root[%d]=%p\n", i, _roots[i]);
         heap_object *p = *_roots[i];
         if (p != NULL) {
-            if (DEBUG) printf("root=%s@%p\n", p->header.metaclass->name, p);
+            if (DEBUG) printf("root=%s@%p\n", p->metaclass->name, p);
             if ( gc_in_heap(p) ) {
                 gc_mark_object(p);
             }
@@ -231,15 +233,15 @@ static void gc_mark_live() {
 
 /* recursively walk object graph starting from p. */
 static void gc_mark_object(heap_object *p) {
-    if (!p->header.marked) {
+    if (!p->marked) {
         int i;
-        if (DEBUG) printf("mark %s@%p\n", p->header.metaclass->name, p);
-        p->header.marked = 1;
+        if (DEBUG) printf("mark %s@%p\n", p->metaclass->name, p);
+        p->marked = 1;
         live_objects[num_live_objects++] = p; // track live
         // check for tracked heap ptrs in this object
-        for (i = 0; i < p->header.metaclass->num_fields; i++) {
-            int offset_of_ptr_field = p->header.metaclass->field_offsets[i];
-            byte *ptr_to_ptr_field = ((byte *) p) + offset_of_ptr_field;
+        for (i = 0; i < p->metaclass->num_fields; i++) {
+            int offset_of_ptr_field = p->metaclass->field_offsets[i];
+            uint8_t *ptr_to_ptr_field = ((uint8_t *) p) + offset_of_ptr_field;
             heap_object **ptr_to_obj_ptr_field = (heap_object **) ptr_to_ptr_field;
             heap_object *target_obj = *ptr_to_obj_ptr_field;
             if (target_obj != NULL) {
@@ -254,7 +256,8 @@ static bool gc_in_heap(heap_object *p) {
 }
 
 /** Allocate size bytes in the heap; if full, gc() */
-static void *gc_alloc_space(int size) {
+static void *gc_alloc_space(size_t size) {
+	size = request2size(size);
     if (next_free + size > end_of_heap) {
         gc(); // try to collect        
         if (next_free + size > end_of_heap) { // try again
@@ -268,31 +271,31 @@ static void *gc_alloc_space(int size) {
 }
 
 static void print_ptr(heap_object *p) {
-    if (p->header.metaclass == &String_metaclass) {
-        printf("%s[%d]@%ld\n", p->header.metaclass->name,
+    if (p->metaclass == &String_metaclass) {
+        printf("%s[%d]@%ld\n", p->metaclass->name,
                 ((String *) p)->length, gc_rel_addr(p));
-    } else printf("%s@%ld\n", p->header.metaclass->name, gc_rel_addr(p));
+    } else printf("%s@%ld\n", p->metaclass->name, gc_rel_addr(p));
 }
 
 static char *ptr_to_str(heap_object *p) {
     char *buf = malloc(200);
-    if (p->header.metaclass == &String_metaclass) {
+    if (p->metaclass == &String_metaclass) {
         String *s = (String *) p;
         sprintf(buf, "%04ld:String[%ld+%d]=\"%s\"\n",
                 gc_rel_addr(p), sizeof (String), ((String *) p)->length + 1, s->str);
     }
     else {
-        sprintf(buf, "%04ld:%s[%d]\n", gc_rel_addr(p), p->header.metaclass->name, gc_object_size(p));
+        sprintf(buf, "%04ld:%s[%d]\n", gc_rel_addr(p), p->metaclass->name, gc_object_size(p));
     }
     return buf;
 }
 
 static int gc_object_size(heap_object *p) {
-    if (p->header.metaclass == &String_metaclass) {
+    if (p->metaclass == &String_metaclass) {
         int n = ((String *) p)->length;
-        return p->header.metaclass->size + n + 1;
+        return p->metaclass->size + n + 1;
     }
-    return p->header.metaclass->size;
+    return p->metaclass->size;
 }
 
 static void gc_dump() {
@@ -320,7 +323,7 @@ static char *long_array_to_str(unsigned long *array, int len) {
     return buffer;
 }
 
-/* Get a string with a char per byte of heap. _=live, .=free.
+/* Get a string with a char per uint8_t of heap. _=live, .=free.
    Chop down to avoid empty space after last live object.
  */
 static char *gc_viz_heap() {
@@ -341,8 +344,8 @@ static char *gc_viz_heap() {
                     start, n, heap_size);
             continue;
         }
-        char *name = p->header.metaclass->name;
-        int nlen = (int) strlen(p->header.metaclass->name);
+        char *name = p->metaclass->name;
+        int nlen = (int) strlen(p->metaclass->name);
         int min = nlen < n ? nlen : n;
         strncpy(&map[start + 1], name, min);
         for (j = start + 1 + nlen; j < start + n - 1; j++) {
@@ -359,7 +362,7 @@ static char *gc_viz_heap() {
 /** Convert pointers in heap to 0..heapsize-1 */
 static unsigned long gc_rel_addr(heap_object *p) {
     if ( p==NULL ) return 0;
-    return ((byte *) p)-start_of_heap;
+    return ((uint8_t *) p)-start_of_heap;
 }
 
 /* qsort address comparison function */
@@ -373,7 +376,7 @@ static void unmark_objects() {
     // done with live_objects, wack it
     int i;
     for (i = 0; i < num_live_objects; i++) {
-        live_objects[i]->header.marked = 0; // turn off bits set during bogus gc_mark
+        live_objects[i]->marked = 0; // turn off bits set during bogus gc_mark
     }
     num_live_objects = 0;
 }
--- mark-and-compact/gc.h
+++ mark-and-compact/gc.h
@@ -7,19 +7,14 @@
 extern "C" {
 #endif
 
-typedef unsigned char byte;
-
-typedef struct { 	// stuff that every instance in the heap must have at the beginning
-	int type; 		// numeric identifier for the type of this heap object
-	byte marked;	// used during the mark phase of garbage collection
-	struct heap_object *forwarded; // where we've moved this object during collection
-	unsigned char mem[]; // nothing allocated; just a label to location after size
+/* stuff that every instance in the heap must have at the beginning */
+typedef struct _heap_object {
+	uint32_t size;  // 31 bits for size and 1 bit for inuse/free; size includes header data
+	uint8_t marked;	// used during the mark phase of garbage collection
+	void (*chase_ptrs)(struct _heap_object *p); // how to chase pointer fields of this type of object
+	struct heap_object *forwarded; 				// where we've moved this object during collection
+	unsigned char mem[]; // nothing allocated; just a label to location of actual instance data
 } heap_object;
-
-typedef struct {
-	uint32_t size;       // 31 bits for size and 1 bit for inuse/free; size includes header data
-	void (*mark)(heap_object *p);
-} type_metadata;
 
 // GC interface
 
@@ -33,7 +28,7 @@ extern void gc_done();
  * to the start of the heap.
  */
 extern void gc();
-extern heap_object *gc_alloc(int type);
+extern heap_object *gc_alloc(size_t size, void (*chase_ptrs)(struct _heap_object *p));
 extern void gc_add_addr_of_root(heap_object **p);
 
 #define gc_begin_func()		int __save = gc_num_roots()
@@ -46,6 +41,26 @@ extern char *gc_get_state();
 extern long gc_heap_highwater();
 extern int gc_num_roots();
 extern void gc_set_num_roots(int roots);
+
+static const size_t WORD_SIZE_IN_BYTES = sizeof(void *);
+static const size_t ALIGN_MASK = WORD_SIZE_IN_BYTES - 1;
+
+/* Pad size n to include header */
+static inline size_t size_with_header(size_t n) {
+	return n + sizeof(heap_object);
+}
+
+/* Align n to nearest word size boundary (4 or 8) */
+static inline size_t align_to_word_boundary(size_t n) {
+	return (n & ALIGN_MASK) == 0 ? n : (n + WORD_SIZE_IN_BYTES) & ~ALIGN_MASK;
+}
+
+/* Convert a user request for n bytes into a size in bytes that has all necessary
+ * header room and is word aligned.
+ */
+static inline size_t request2size(size_t n) {
+	return align_to_word_boundary(size_with_header(n));
+}
 
 #ifdef __cplusplus
 }
